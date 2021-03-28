d8accbb01240e7218b7cf962b3c1495eeffc9923
Mateusz Olszewski <olszef@gmail.com>
2021-03-28 22:46:31 +0200

    Initial commit

---
+++ src/main/java/com/olszef/spanishplatform/SpanishPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.olszef.spanishplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpanishPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpanishPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olszef/spanishplatform/SpanishPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olszef.spanishplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpanishPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

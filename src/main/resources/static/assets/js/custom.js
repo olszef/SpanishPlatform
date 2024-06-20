de9331f891e38ffc06f926d1b9e369ffe2146d8b
Mateusz Olszewski <olszef@gmail.com>
2024-06-20 19:24:33 +0200

    Arrange CSS and JS file in a consistent way
    - Put page specific CSS and JS code to seperate files and link them in HTMLs
    - Leave in "custom" CSS and JS files only common code
    - Put the commoon "custom" CSS file in an HTML fragment and include the fragment in all pages

--- src/main/resources/static/assets/js/custom.js
+++ src/main/resources/static/assets/js/custom.js
@@ -1,32 +1,3 @@
-function swapTranslationLanguages() {
-
-	var languageFromElement = document.getElementById("languageFrom");
-	var languageToElement = document.getElementById("languageTo");
-	
-	var languageFromId = languageFromElement.value;
-	var languageToId = languageToElement.value;
-	
-	languageFromElement.value = languageToId;
-	languageToElement.value = languageFromId;
-}
-
-function forceSpanishLanguage(element) {
-
-	var otherOptionElement;
-	
-	if (element.id == "languageFrom") {
-		otherOptionElement = document.getElementById("languageTo");
-	} else {
-		otherOptionElement = document.getElementById("languageFrom");
-	}
-	
-	if (element.value == 1) {
-		otherOptionElement.value = 2;
-	} else {
-		otherOptionElement.value = 1;
-	}
-}
-
 $(document).ready(function() {
 	
 	var menuTabsAll = $('#menu li span');
